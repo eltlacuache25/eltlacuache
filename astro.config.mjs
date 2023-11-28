efe44df919ba0d68c0983e52b984547b0b15967d
eltlacuache25 <152246947+eltlacuache25@users.noreply.github.com>
2023-11-28 17:38:47 +0000

    modficacion deploy dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://eltlacuachenews.com/',
+  site: 'https://eltlacuache25.github.io',
   base: '/eltlacuache',
   //output: "server",
   //adapter: netlify()
