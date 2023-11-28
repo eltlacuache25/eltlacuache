94b31ffdc5a66a35dc5891a65c3c13b27fc41a49
angel <angel1521ramos@gmail.com>
2023-11-27 23:24:12 -0600

    deploy sin errores

--- dist/pages/posts/post_2.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-export { onRequest } from '../../_empty-middleware.mjs';
-
-const page = () => import('../../chunks/pages/post_2_3a98cad9.mjs');
-
-export { page };
--- dist/pages/posts/postslayout.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-export { onRequest } from '../../_empty-middleware.mjs';
-
-const page = () => import('../../chunks/pages/PostsLayout_571c54ff.mjs').then(n => n.P);
-
-export { page };
